05199f05d1fa3fd27c9c6d10cf6a56e91a9ad2bd
Craig MacKenzie <mackenzie.craiga@gmail.com>
2017-04-28 23:05:09 +0100

    added even more test cases

--- src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
+++ src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
@@ -8,13 +8,53 @@ import static org.junit.Assert.*;
  * Created by craig on 28/04/17.
  */
 public class StringHelperTest{
+    StringHelper helper = new StringHelper();
 
     @Test
-    public void test(){
-        StringHelper helper = new StringHelper();
+    public void testtruncateAInFirst2Positions_Ainfirst2positions(){
         assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
-        assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
+
         //expected, actual
+    }
+
+    @Test
+    public void testtruncateAInFirst2Positions2_1AInFirstPosition(){
+        assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
+    }
+
+    @Test
+    public void testtruncateAInFirst2Positions2_NoAPresent(){
+        assertEquals("CDEF", helper.truncateAInFirst2Positions("CDEF"));
+    }
+
+    @Test
+    public void testtruncateAInFirst2Positions2_2AsInLastPositions(){
+        assertEquals("CDAA", helper.truncateAInFirst2Positions("CDAA"));
+    }
+
+    @Test
+    public void testAreFirstAndLastTwoCharactersTheSame_messageExample(){
+        assertFalse("The condition failed", false);
+    }
+
+    @Test
+    public void testAreFirstAndLastTwoCharactersTheSame_BasicNegativeScenario(){
+        assertFalse(helper.areFirstAndLastTwoCharactersTheSame("ABCD"));
+    }
+
+    @Test
+    public void testAreFirstAndLastTwoCharactersTheSame_BasicPositiveScenario(){
+        assertTrue(helper.areFirstAndLastTwoCharactersTheSame("ABAB"));
+    }
+
+    @Test
+    public void testAreFirstAndLastTwoCharactersTheSame_OnlyTwoCharacters(){
+        assertTrue(helper.areFirstAndLastTwoCharactersTheSame("AB"));
+    }
+
+    @Test
+    public void testAreFirstAndLastTwoCharactersTheSame_SingleCharacterFalse(){
+        assertFalse(helper.areFirstAndLastTwoCharactersTheSame("A"));
     }
 
 }
