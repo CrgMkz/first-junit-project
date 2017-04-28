e0afc790d0a4c05549dfacc1e8452d24cb5df335
Craig MacKenzie <mackenzie.craiga@gmail.com>
2017-04-28 22:02:23 +0100

    first commit

---
+++ src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
@@ -0,0 +1,17 @@
+package com.in28minutes.junit.helper;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by craig on 28/04/17.
+ */
+public class StringHelperTest{
+
+    @Test
+    public void test(){
+        fail("Not yet implemented");
+    }
+
+}
